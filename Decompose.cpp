9ce69e15fa4e9a052d8b0157f42ed260a72e8e41
AndreyCodes <70113676+AndreyCodes@users.noreply.github.com>
2020-08-24 16:14:40 +0300

    Update Decompose.cpp

--- Decompose.cpp
+++ Decompose.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -8,7 +8,7 @@ using namespace std;
 class PrimeDecomp
 {
 private:
-    static vector<int> primes(int size)   //Решето Эратосфена// 0 означает, что число с данным индексом простое//
+    static vector<int> primes(int size)
     {
         vector<int> a;
         a.resize(size+1);
@@ -20,7 +20,7 @@ private:
     }
 
 
-    static string createForm(int x, int power) //Создание формы для печати
+    static string createForm(int x, int power)
     {
         if (power == 1) return "(" + to_string(x) + ")";
         return "(" + to_string(x) + "**" + to_string(power) + ")";
@@ -28,21 +28,25 @@ private:
 
 
 public:
-    static std::string factors(const int x) //Алгоритм разложение на множители
+    static std::string factors(const int x)
     {
         string result = "";
-        vector<int> pr = primes(x);         //Решето чисел [2,x]
+        int last = x;
+        vector<int> pr = primes(sqrt(x));
         for (int i = 2; i < pr.size(); i++)
         {
-            if (pr[i] == 0) // пробуем каждое простое число
+            if (pr[i] == 0)
             {
                 int temp = x;
-                int j = 0; //степень простого числа
-                for (; temp % i == 0; temp/=i, j++);  
+                int j = 0;
+                for (; temp % i == 0; temp/=i, j++);
                 if (j == 0) continue;
                 result += createForm(i, j);
+                for (int n = 0; n < j; n++) last /= i;
             }
         }
+        if(last!=1)
+        result += createForm(last, 1);
         return result;
     }
 };
